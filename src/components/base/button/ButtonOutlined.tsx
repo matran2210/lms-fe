e88dc97bdbaf8660260883a8e22b2a300974f09e
minhnh <minhnh@onextdigital.com>
2023-11-09 09:18:44 +0700

    feat: create button components

---
+++ src/components/base/button/ButtonOutlined.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { IButtonProps } from 'src/type'
+
+const ButtonOutlined = ({
+  title,
+  onClick,
+  className = '',
+  link,
+}: IButtonProps) => {
+  if (link)
+    return (
+      <a href={link} className={`${className} btn btn-light`}>
+        <i className="ki-outline ki-check fs-3 d-none"></i>
+        <span className="indicator-label">{title}</span>
+        <span className="indicator-progress">
+          Please wait...
+          <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+        </span>
+      </a>
+    )
+
+  return (
+    <div className={`${className} btn btn-light me-3`} onClick={onClick}>
+      {title}
+    </div>
+  )
+}
+
+export default ButtonOutlined
