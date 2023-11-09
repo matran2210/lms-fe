16f8eca38c3b0112f2a184e876e3cf23ce9031a5
minhnh <minhnh@onextdigital.com>
2023-11-09 17:56:38 +0700

    feat: update button component styles + show btn demo in home

--- src/components/base/button/ButtonText.tsx
+++ src/components/base/button/ButtonText.tsx
@@ -19,8 +19,8 @@ const ButtonText = ({
     size === 'small' ? 'py-2' : size === 'medium' ? 'py-2' : 'py-2.8'
   let paddingHorizontalClass =
     size === 'small' ? 'px-7' : size === 'medium' ? 'px-8' : 'px-9'
-  let fullWidthClass = full ? 'block' : 'inline-block w-fit'
-  let componentClass = `${className} text-center ${fullWidthClass} ${paddingVerticalClass} ${paddingHorizontalClass} mx-0 text-bw-1 ${textSizeClass} font-semibold underline cursor-pointer`
+  let fullWidthClass = full ? 'block w-full' : 'inline-block w-fit'
+  let componentClass = `${className} text-center ${fullWidthClass} ${paddingVerticalClass} ${paddingHorizontalClass} text-bw-1 ${textSizeClass} font-semibold underline cursor-pointer`
 
   if (link)
     return (
