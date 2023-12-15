148072e967e43e406ab17bc0a9634e049f60fa74
Phuc <phucnguyen.convertdigital@gmail.com>
2023-12-15 14:35:06 +0700

    build: 404 page, fix login and font

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,8 +1,40 @@
 import { LAYOUT } from '@utils/constants'
 import React from 'react'
+import SappButton from '@components/base/button/SappButton'
+import Link from 'next/link'
+import { PageLink } from 'src/constants'
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>
+  return (
+    <div className="h-screen flex items-center justify-center flex-col flex-nowrap overflow-y-auto p-4 text-center">
+      <img
+        className="h-max"
+        src="/assets/images/image_404.jpg"
+        loading="lazy"
+        alt="Image_404"
+        width="320"
+        height="260"
+      />
+      <h1 className="text-2xl md:text-4xl text-bw-1 font-bold mt-10">
+        PAGE NOT FOUND
+      </h1>
+      <span className="text-base text-gray-1 max-w-dl px-4 mt-3">
+        We are very sorry for inconvenience. It looks like you’re trying to
+        access a page that was has been deleted or never even existed.
+      </span>
+      <Link href={PageLink.DASHBOARD}>
+        <div className="mt-8">
+          <SappButton
+            title="Back to Home"
+            full={false}
+            className="px-8 md:px-22"
+            size="lager"
+            type="submit"
+          />
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 // eslint-disable-next-line import/no-unused-modules
