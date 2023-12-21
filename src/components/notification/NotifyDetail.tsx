eac61f3e29d75ad93b6ea9f4dd415f2ad9e9acde
Phuc <phucnguyen.convertdigital@gmail.com>
2023-12-21 16:20:11 +0700

    fix: update desgin & fix bugs

--- src/components/notification/NotifyDetail.tsx
+++ src/components/notification/NotifyDetail.tsx
@@ -13,28 +13,8 @@ const NotifyDetail = ({ notifyDetail }: IProps) => {
   const dispatch = useAppDispatch()
   return (
     <div key={notifyDetail?.id} className="w-full">
-      <div className="w-full relative flex items-center border-b border-default pb-5">
-        <Image
-          src={blankAvatar}
-          alt="avatar"
-          className="rounded-full"
-          width={56}
-          height={56}
-          layout="fixed"
-          objectFit={'cover'}
-        />
-        <div className="block pl-5">
-          <h4
-            className="text-base text-bw-1 mb-1"
-            dangerouslySetInnerHTML={{ __html: notifyDetail?.title }}
-          ></h4>
-          <p className="text-gray-1 text-medium-sm text-left">
-            {calculateTimeAgo(notifyDetail?.updated_at)}
-          </p>
-        </div>
-      </div>
       <div
-        className="pt-5"
+        className="text-bw-1"
         dangerouslySetInnerHTML={{ __html: notifyDetail?.content }}
       ></div>
     </div>
