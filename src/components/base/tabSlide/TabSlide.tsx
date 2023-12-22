8098090b4b90b950d842931baf771378c5b4bead
hoangdat <hoangdat@hoangdats-MacBook-Air.local>
2023-12-22 15:10:06 +0700

    refactor: ui, slide-tab

--- src/components/base/tabSlide/TabSlide.tsx
+++ src/components/base/tabSlide/TabSlide.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useRef, useState } from 'react'
+import { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
 import Pagination from '../pagination/Pagination'
 import PageLink from '../pagination/PageLink'
 import ArrowIcon from '../pagination/ArrowIcon'
@@ -9,6 +9,8 @@ interface IProps {
   optionShowAll?: ReactNode
   currentTab: string
   handleChangeTab?: any
+  activeShowAll: boolean
+  setActiveShowAll: any
 }
 
 const TabSlide = ({
@@ -17,10 +19,11 @@ const TabSlide = ({
   optionShowAll,
   currentTab,
   handleChangeTab,
+  activeShowAll,
+  setActiveShowAll,
 }: IProps) => {
   const elementRef = useRef(null) as any
 
-  const [activeShowAll, setActiveShowAll] = useState<boolean>(true)
   useEffect(() => {
     if (elementRef?.current) {
       elementRef.current.scrollTo(
@@ -33,6 +36,18 @@ const TabSlide = ({
       )
     }
   }, [currentTab, elementRef?.current])
+  const renderTab = useMemo(() => {
+    let arr = [] as any
+    for (let i = 1; i <= data.length; i += 2) {
+      if (i === data.length) {
+        arr.push([data[i - 1]])
+      } else {
+        arr.push([data[i - 1], data[i]])
+      }
+    }
+    return arr
+  }, [data])
+
   return (
     <ul
       className={`pagination flex items-center flex-wrap w-full gap-3 min-h-[40px]`}
@@ -40,7 +55,7 @@ const TabSlide = ({
     >
       <div
         className={`${
-          activeShowAll
+          !activeShowAll
             ? 'relative w-[calc(100%-141px)] mx-7'
             : ' flex items-center gap-6 w-full'
         }`}
@@ -48,7 +63,7 @@ const TabSlide = ({
         {data.length > 0 && (
           <div
             className={`${
-              activeShowAll && 'absolute top-0.5 -left-3 -translate-x-full'
+              !activeShowAll && 'absolute top-0.5 -left-3 -translate-x-full'
             }`}
           >
             <PageLink
@@ -71,35 +86,70 @@ const TabSlide = ({
           </div>
         )}
         <div
-          className={`${
-            activeShowAll
-              ? 'flex gap-2 overflow-auto w-full'
-              : 'flex items-center gap-2 flex-wrap flex-1'
-          }`}
+          className={`${'flex gap-2 overflow-auto w-full'}`}
           ref={elementRef}
         >
-          {data.map((pageNum: any, idx: any) => (
-            <PageLink
-              key={pageNum.id}
-              active={currentTab === pageNum.id}
-              // disabled={isNaN(pageNum)}
-              onClick={() => {
-                if (setCurrentTab !== undefined) {
-                  handleChangeTab(pageNum.id)
-                }
-              }}
-              isViewedProp={pageNum.viewed}
-              isFlagedProp={pageNum.flaged}
-              //   type={type}
-            >
-              {pageNum.index + 1}
-            </PageLink>
-          ))}
+          {!activeShowAll
+            ? data.map((pageNum: any, idx: any) => (
+                <PageLink
+                  key={pageNum.id}
+                  active={currentTab === pageNum.id}
+                  // disabled={isNaN(pageNum)}
+                  onClick={() => {
+                    if (setCurrentTab !== undefined) {
+                      handleChangeTab(pageNum.id)
+                    }
+                  }}
+                  isViewedProp={pageNum.viewed}
+                  isFlagedProp={pageNum.flaged}
+                  //   type={type}
+                >
+                  {pageNum.index + 1}
+                </PageLink>
+              ))
+            : renderTab.map((pageNum: any, idx: number) => {
+                return (
+                  <div className="flex flex-col gap-2" key={idx}>
+                    <PageLink
+                      key={pageNum[0].id}
+                      active={currentTab === pageNum[0].id}
+                      // disabled={isNaN(pageNum)}
+                      onClick={() => {
+                        if (setCurrentTab !== undefined) {
+                          handleChangeTab(pageNum[0].id)
+                        }
+                      }}
+                      isViewedProp={pageNum[0].viewed}
+                      isFlagedProp={pageNum[0].flaged}
+                      //   type={type}
+                    >
+                      {pageNum[0].index + 1}
+                    </PageLink>
+                    {pageNum[1] && (
+                      <PageLink
+                        key={pageNum[1].id}
+                        active={currentTab === pageNum[1].id}
+                        // disabled={isNaN(pageNum)}
+                        onClick={() => {
+                          if (setCurrentTab !== undefined) {
+                            handleChangeTab(pageNum[1].id)
+                          }
+                        }}
+                        isViewedProp={pageNum[1].viewed}
+                        isFlagedProp={pageNum[1].flaged}
+                        //   type={type}
+                      >
+                        {pageNum[1].index + 1}
+                      </PageLink>
+                    )}
+                  </div>
+                )
+              })}
         </div>
         {data.length > 0 && (
           <div
             className={`${
-              activeShowAll && 'absolute top-0.5 -right-3 translate-x-full'
+              !activeShowAll && 'absolute top-0.5 -right-3 translate-x-full'
             }`}
           >
             <PageLink
@@ -121,17 +171,17 @@ const TabSlide = ({
         )}
 
         <div className="flex items-center">
-          {!activeShowAll && optionShowAll}
+          {activeShowAll && optionShowAll}
           <div
             className={`ml-6 text-sm leading-4.5 text-bw-1 underline font-semibold cursor-pointer w-max ${
-              activeShowAll && 'absolute -right-28 top-1/2 -translate-y-1/2 '
+              !activeShowAll && 'absolute -right-28 top-1/2 -translate-y-1/2 '
             }`}
             onClick={() => {
               // setPageNums(activeShowAll ? arrPage : getPagination)
               setActiveShowAll(!activeShowAll)
             }}
           >
-            {activeShowAll ? 'Show All' : 'Show Less'}
+            {!activeShowAll ? 'Show All' : 'Show Less'}
           </div>
         </div>
       </div>
