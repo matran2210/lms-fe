68025df333edf52031eecaea3aa092b89a7ad6a2
hoangdat <hoangdat@hoangdats-MacBook-Air.local>
2023-12-26 18:28:41 +0700

    refactor: add split slide tabs

--- src/components/base/tabSlide/TabSlide.tsx
+++ src/components/base/tabSlide/TabSlide.tsx
@@ -2,6 +2,7 @@ import { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
 import Pagination from '../pagination/Pagination'
 import PageLink from '../pagination/PageLink'
 import ArrowIcon from '../pagination/ArrowIcon'
+import { QUESTION_TYPES } from 'src/constants'
 
 interface IProps {
   data: Array<any>
@@ -25,7 +26,7 @@ const TabSlide = ({
   const elementRef = useRef(null) as any
 
   useEffect(() => {
-    if (elementRef?.current) {
+    if (elementRef?.current && !activeShowAll) {
       elementRef.current.scrollTo(
         elementRef?.current.offsetWidth *
           Math.floor(
@@ -36,18 +37,64 @@ const TabSlide = ({
       )
     }
   }, [currentTab, elementRef?.current])
+  // const renderTab = useMemo(() => {
+  //   let arr = [] as any
+  //   for (let i = 1; i <= data.length; i += 2) {
+  //     if (i === data.length) {
+  //       arr.push([data[i - 1]])
+  //     } else {
+  //       arr.push([data[i - 1], data[i]])
+  //     }
+  //   }
+  //   return arr
+  // }, [data])
   const renderTab = useMemo(() => {
     let arr = [] as any
-    for (let i = 1; i <= data.length; i += 2) {
-      if (i === data.length) {
-        arr.push([data[i - 1]])
-      } else {
-        arr.push([data[i - 1], data[i]])
+    let i = 1
+    let splited = false
+    let splitedPosition
+    for (let e of data) {
+      if (e.qType === QUESTION_TYPES.ESSAY) {
+        splitedPosition = e.index
+        break
       }
     }
+    while (i <= data.length) {
+      if (i === data.length) {
+        if (splited === false) {
+          arr.push([data[i - 1]])
+        } else {
+          arr.push([undefined, data[i - 1]])
+        }
+      } else {
+        if (data[i].index === splitedPosition && splited == false) {
+          arr.push([data[i - 1], 'split'])
+          i--
+          splited = true
+        } else {
+          if (splited === false) {
+            arr.push([data[i - 1], data[i]])
+          } else {
+            arr.push([data[i], data[i - 1]])
+          }
+        }
+      }
+      i += 2
+    }
+    // for (let i = 1; i <= data.length ; i += 2) {
+
+    // }
     return arr
   }, [data])
 
+  const firstEssayPosition = useMemo(() => {
+    for (let e of data) {
+      if (e.qType === QUESTION_TYPES.ESSAY) {
+        return e.index
+      }
+    }
+    return undefined
+  }, [data])
   return (
     <ul
       className={`pagination flex items-center flex-wrap w-full gap-3 min-h-[40px]`}
@@ -90,60 +137,102 @@ const TabSlide = ({
           ref={elementRef}
         >
           {!activeShowAll
-            ? data.map((pageNum: any, idx: any) => (
-                <PageLink
-                  key={pageNum.id}
-                  active={currentTab === pageNum.id}
-                  // disabled={isNaN(pageNum)}
-                  onClick={() => {
-                    if (setCurrentTab !== undefined) {
-                      handleChangeTab(pageNum.id)
-                    }
-                  }}
-                  isViewedProp={pageNum.viewed}
-                  isFlagedProp={pageNum.flaged}
-                  //   type={type}
-                >
-                  {pageNum.index + 1}
-                </PageLink>
-              ))
-            : renderTab.map((pageNum: any, idx: number) => {
-                return (
-                  <div className="flex flex-col gap-2" key={idx}>
+            ? data.map((pageNum: any, idx: any) =>
+                firstEssayPosition !== undefined &&
+                pageNum.index === firstEssayPosition ? (
+                  <div className="flex" key={pageNum.id}>
+                    <div className="h-full border me-2"></div>
                     <PageLink
-                      key={pageNum[0].id}
-                      active={currentTab === pageNum[0].id}
+                      key={pageNum.id}
+                      active={currentTab === pageNum.id}
                       // disabled={isNaN(pageNum)}
                       onClick={() => {
                         if (setCurrentTab !== undefined) {
-                          handleChangeTab(pageNum[0].id)
+                          handleChangeTab(pageNum.id)
                         }
                       }}
-                      isViewedProp={pageNum[0].viewed}
-                      isFlagedProp={pageNum[0].flaged}
+                      isViewedProp={pageNum.viewed}
+                      isFlagedProp={pageNum.flaged}
                       //   type={type}
                     >
-                      {pageNum[0].index + 1}
+                      {pageNum.index + 1}
                     </PageLink>
-                    {pageNum[1] && (
-                      <PageLink
-                        key={pageNum[1].id}
-                        active={currentTab === pageNum[1].id}
-                        // disabled={isNaN(pageNum)}
-                        onClick={() => {
-                          if (setCurrentTab !== undefined) {
-                            handleChangeTab(pageNum[1].id)
-                          }
-                        }}
-                        isViewedProp={pageNum[1].viewed}
-                        isFlagedProp={pageNum[1].flaged}
-                        //   type={type}
-                      >
-                        {pageNum[1].index + 1}
-                      </PageLink>
+                  </div>
+                ) : (
+                  <PageLink
+                    key={pageNum.id}
+                    active={currentTab === pageNum.id}
+                    // disabled={isNaN(pageNum)}
+                    onClick={() => {
+                      if (setCurrentTab !== undefined) {
+                        handleChangeTab(pageNum.id)
+                      }
+                    }}
+                    isViewedProp={pageNum.viewed}
+                    isFlagedProp={pageNum.flaged}
+                    //   type={type}
+                  >
+                    {pageNum.index + 1}
+                  </PageLink>
+                ),
+              )
+            : renderTab.map((pageNum: any, idx: number) => {
+                // if (pageNum) {
+                return (
+                  <div className="flex flex-col gap-2" key={idx}>
+                    {pageNum[0] ? (
+                      pageNum[0] !== 'split' ? (
+                        <PageLink
+                          key={pageNum[0].id}
+                          active={currentTab === pageNum[0].id}
+                          // disabled={isNaN(pageNum)}
+                          onClick={() => {
+                            if (setCurrentTab !== undefined) {
+                              handleChangeTab(pageNum[0].id)
+                            }
+                          }}
+                          isViewedProp={pageNum[0].viewed}
+                          isFlagedProp={pageNum[0].flaged}
+                          //   type={type}
+                        >
+                          {pageNum[0].index + 1}
+                        </PageLink>
+                      ) : (
+                        <div className="flex items-center h-full justify-center">
+                          <div className="h-full border w-[1px]"></div>
+                        </div>
+                      )
+                    ) : (
+                      <div className="h-full w-[40px]"></div>
+                    )}
+                    {pageNum[1] ? (
+                      pageNum[1] !== 'split' ? (
+                        <PageLink
+                          key={pageNum[1].id}
+                          active={currentTab === pageNum[1].id}
+                          // disabled={isNaN(pageNum)}
+                          onClick={() => {
+                            if (setCurrentTab !== undefined) {
+                              handleChangeTab(pageNum[1].id)
+                            }
+                          }}
+                          isViewedProp={pageNum[1].viewed}
+                          isFlagedProp={pageNum[1].flaged}
+                          //   type={type}
+                        >
+                          {pageNum[1].index + 1}
+                        </PageLink>
+                      ) : (
+                        <div className="flex items-center h-full justify-center">
+                          <div className="h-full border w-[1px]"></div>
+                        </div>
+                      )
+                    ) : (
+                      <div className="h-full w-[40px]"></div>
                     )}
                   </div>
                 )
+                // }
               })}
         </div>
         {data.length > 0 && (
