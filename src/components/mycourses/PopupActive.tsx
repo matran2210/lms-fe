908f471cfef8bb4d382e5cfd165bc25b99aa5f27
Phuc <phucnguyen.convertdigital@gmail.com>
2024-01-12 11:43:23 +0700

    fix: course UI

--- src/components/mycourses/PopupActive.tsx
+++ src/components/mycourses/PopupActive.tsx
@@ -25,7 +25,7 @@ const PopupActive = ({ open, setOpen, activeCourse }: IProps) => {
       handleCancel={handleCancel}
       cancelButtonClass={'no-underline'}
       showHeader={false}
-      refClass="p-6 md:py-[70px] md:px-19 flex flex-col animate-jump-in relative transform bg-white text-left shadow-xl transition-all overflow-y-auto"
+      refClass="p-6 md:p-8 3xl:py-[70px] 3xl:px-19 flex flex-col animate-jump-in relative transform bg-white text-left shadow-xl transition-all"
       size="max-w-[646px]"
       footerButtonClassName="flex flex-col-reverse gap-8"
       childClass="flex flex-col justify-center items-center"
@@ -35,6 +35,7 @@ const PopupActive = ({ open, setOpen, activeCourse }: IProps) => {
       closeAfterSubmit={true}
       buttonSize="extra"
       scrollbale={false}
+      confirmOnclose={true}
     >
       <div className="mb-6">
         <ActiveIcon />
@@ -42,7 +43,7 @@ const PopupActive = ({ open, setOpen, activeCourse }: IProps) => {
       <div className="text-2xl md:text-4xl text-bw-1 font-semibold">
         Active Course?
       </div>
-      <div className="text-medium-sm text-gray-1 text-center mt-4 mb-11">
+      <div className="text-medium-sm text-gray-1 text-center mt-4 mb-1 2xl:mb-11">
         You will have 60 days from the activation date to study this course
       </div>
     </SappModal>
