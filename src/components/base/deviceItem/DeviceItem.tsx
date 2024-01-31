58d88032dd3786b4316b42181e7db04a157d0598
dunght.aht <khactuannguyen54@gmail.com>
2024-01-31 11:18:42 +0700

    fix: fix useMemo format

--- src/components/base/deviceItem/DeviceItem.tsx
+++ src/components/base/deviceItem/DeviceItem.tsx
@@ -14,23 +14,23 @@ const DeviceItem = ({ data }: any) => {
       }
     }
   }, [data?.user_agent?.osName])
-  // const formattedDate = useMemo(() => {
-  //   if (data.created_at) {
-  //     return formatDistanceToNow(new Date(data.created_at), { addSuffix: true })
-  //   }
-  // }, [data.created_at])
-  const loginTime = new Date(data.created_at)
-  const currentTime = new Date()
+  const formattedDate = useMemo(() => {
+    if (data.created_at) {
+      const loginTime = new Date(data.updated_at)
+      const currentTime = new Date()
 
-  const timeDifference = currentTime.getTime() - loginTime.getTime()
+      const timeDifference = currentTime.getTime() - loginTime.getTime()
 
-  let formattedTime
+      let formattedTime
 
-  if (timeDifference < 24 * 60 * 60 * 1000) {
-    formattedTime = formatDistanceToNow(loginTime, { addSuffix: true })
-  } else {
-    formattedTime = format(loginTime, 'HH:mm:ss dd/MM/yyyy')
-  }
+      if (timeDifference < 24 * 60 * 60 * 1000) {
+        formattedTime = formatDistanceToNow(loginTime, { addSuffix: true })
+      } else {
+        formattedTime = format(loginTime, 'HH:mm:ss dd/MM/yyyy')
+      }
+    }
+    return null
+  }, [data.created_at])
   return (
     <div className="flex items-center py-5 px-6 hover:bg-secondary sapp-hover-device-item gap-4">
       <div className="border border-gray-1 flex sapp-logo bg-gray-4 box-border w-12 h-12 items-center justify-center">
@@ -48,7 +48,7 @@ const DeviceItem = ({ data }: any) => {
           )}
         </div>
         <div className="flex items-center gap-2">
-          <div className="text-xsm text-gray-1">{formattedTime}</div>
+          <div className="text-xsm text-gray-1">{formattedDate}</div>
           <div className="bg-gray-1 rounded-full w-[4px] h-[4px]"></div>
           <div className="text-xsm text-gray-1">{data.ip}</div>
           {data.location && (
