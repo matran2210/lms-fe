564edb0633ca9e6fbe6a33cdd1e623606cd5396d
huytu20 <tudh@sapp.edu.vn>
2024-03-21 18:00:21 +0700

    fix: update loading button

--- src/components/base/button/SappButton.tsx
+++ src/components/base/button/SappButton.tsx
@@ -36,7 +36,7 @@ const PADDINGS = {
 }
 
 const COLOR_LOADING = {
-  primary: '',
+  primary: 'bg-white',
   info: 'bg-white',
   success: 'bg-white',
   secondary: 'bg-bw-1',
@@ -85,7 +85,7 @@ const SappButton = ({
       disabled={disabled || loading}
     >
       {loading ? (
-        <>
+        <div className="flex justify-center">
           <span className="sr-only">Loading...</span>
           <div
             className={`h-2 w-2 rounded-full animate-bounce [animation-delay:-0.3s] ${COLOR_LOADING[color]}`}
@@ -96,10 +96,9 @@ const SappButton = ({
           <div
             className={`h-2 w-2 rounded-full animate-bounce ${COLOR_LOADING[color]}`}
           ></div>
-        </>
+        </div>
       ) : (
         <span className={`${loading ? 'invisible' : ''} ${childClass}`}>
-          {' '}
           {title}
         </span>
       )}
