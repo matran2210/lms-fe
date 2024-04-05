5b0e854311ed2b8a9ef08881f4cae00c0620b872
loc <nguyengialoc7@gmail.com>
2024-04-05 11:32:53 +0700

    fix: [MASPP2402LMS-17] - fix style test result

--- src/pages/courses/test/test-result/acca/chartACCAScore.tsx
+++ src/pages/courses/test/test-result/acca/chartACCAScore.tsx
@@ -13,11 +13,11 @@ interface IProps {
 
 const ChartACCAScore = ({ data }: IProps) => {
   return (
-    <div className="block bg-white mb-6 pt-6 pb-5 pr-5 pl-6 xl:pl-[99px] shadow-sidebar max-w-[1144px]">
+    <div className="block bg-white xl:mb-6 mb-4 pt-6 pb-5 pr-5 pl-6 xl:pl-[99px] shadow-sidebar max-w-[1144px] h-[161.8px]">
       <div className="text-lg-xl xl:text-xl font-semibold xl:font-medium text-bw-1 mb-5">
         Your Score by Topic
       </div>
-      <div className="w-full overflow-x-auto">
+      <div className="w-full overflow-x-auto pb-3">
         <div className="flex flex-row gap-14 flex-start">
           {data?.map((item: any) => (
             <div
