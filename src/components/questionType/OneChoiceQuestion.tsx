1542259a95057045c9a1f061a1caf41eeb1c84b4
huytu20 <tudh@sapp.edu.vn>
2024-04-09 16:10:38 +0700

    fix: sort answers

--- src/components/questionType/OneChoiceQuestion.tsx
+++ src/components/questionType/OneChoiceQuestion.tsx
@@ -17,6 +17,10 @@ export type IPreviewProp = {
   solution?: any
   allowUnHighLight?: boolean
 }
+
+type IAnswers = {
+  answer_position: number
+}
 const OneChoiceQuestion = ({
   data,
   control,
@@ -42,7 +46,7 @@ const OneChoiceQuestion = ({
     let answers = []
     if (data?.answers) {
       data.answers.sort(
-        (a: any, b: any) => a.answer_position - b.answer_position,
+        (a: IAnswers, b: IAnswers) => a.answer_position - b.answer_position,
       )
       for (let e of data?.answers) {
         answers.push({ label: e.answer, value: e.id })
