13243dfa9e9f6bd8d10de867fde948829480972e
huytu20 <tudh@sapp.edu.vn>
2024-05-06 18:57:17 +0700

    fix/SAPP-1153: UI next activity

--- src/components/layout/SinglePage/index.tsx
+++ src/components/layout/SinglePage/index.tsx
@@ -7,6 +7,6 @@ interface LayoutProps {
 export default function SinglePageLayout(props: LayoutProps): ReactElement {
     const { children } = props
     return (
-        <div className="overflow-hidden">{children}</div>
+        <div className="overflow-hidden bg-gray-3 h-screen">{children}</div>
     )
 }
