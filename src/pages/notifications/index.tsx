3f9c0ab1fe04656bd592523ab93db4ff15233fb6
huytu20 <tudh@sapp.edu.vn>
2024-06-03 17:27:22 +0700

    fix(SAPP-2357): error play video

--- src/pages/notifications/index.tsx
+++ src/pages/notifications/index.tsx
@@ -143,11 +143,9 @@ const Notifications = () => {
 
   return (
     <>
-      {loadingRedirect && (
-        <div className="fixed left-0 top-0 right-0 bottom-0 w-screen h-screen backdrop-blur-sm flex justify-center items-center z-[9999]">
-          Loading
-        </div>
-      )}
+      {/* {loadingRedirect && (
+        <SappLoading />
+      )} */}
       <div className="header bg-white border-b border-default px-4 lg:px-20">
         <div className="max-w-xxl my-0 mx-auto flex py-4.5">
           <SearchForm
