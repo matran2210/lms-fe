8896225653722b68ddda0159768abb4b143c4458
DESKTOP-I674IN4\SAPP <phunt@sapp.edu.vn>
2024-07-12 13:32:00 +0700

    [SAPP-3181]Tắt thông báo đi và login lại thì thông báo không hiển thị nữa

--- src/components/layout/DashboardLayout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout/DashboardLayout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
   const toggleDrawer = () => {
     setOpened((prev) => !prev)
   }
-  const { openPinned } = usePinnedNotifyContext()
+  const { openPinned, pinnedNotifications } = usePinnedNotifyContext()
 
   const guideStatus = useAppSelector((state) => state.userGuideReducer?.status)
 
@@ -29,13 +29,13 @@ export default function DashboardLayout({
         toggleDrawer={toggleDrawer}
         className={`menu-sidebar-left fixed top-0 md:left-0 h-screen bg-white shadow-sidebar w-20 max-w-screen ${
           openDrawer ? 'opacity-5' : ''
-        } ${guideStatus ? '' : 'overflow-hidden'} ${openPinned ? 'pt-12' : ''}`}
+        } ${guideStatus ? '' : 'overflow-hidden'} ${openPinned && pinnedNotifications?.data?.content ? 'pt-12' : ''}`}
         setOpenResource={setOpenResource}
       />
       <div className="w-full min-h-screen">
         {/* <Header isOpened={isOpened} toggleDrawer={toggleDrawer} /> */}
         {/* <div> */}
-        <div className={`${openPinned ? 'pt-12' : ''} bg-gray-4 min-h-full`}>
+        <div className={`${openPinned && pinnedNotifications?.data?.content ? 'pt-12' : ''} bg-gray-4 min-h-full`}>
           <div className="ml-0 md:ml-20 sapp-loading">{children}</div>
         </div>
         {/* </div> */}
