6276a565e839f4a7ca8038f603bfe6f5382fd2bf
andnh <25-sapp.fe.andnh@users.noreply.git.sapp.edu.vn>
2024-08-21 15:59:15 +0700

    feat: [SAPP-3986] modal mobile not friendly

---
+++ src/components/base/modal/ModalMobile.tsx
@@ -0,0 +1,73 @@
+import { SadIcon } from '@assets/icons'
+import { Modal } from 'antd'
+import React, { useEffect, useState } from 'react'
+import ButtonPrimary from '../button/ButtonPrimary'
+import { createPortal } from 'react-dom'
+import { isMobileExtensive } from '@utils/helpers'
+
+const ModalMobile = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const accessToken = localStorage.getItem('actToken')
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleOk = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
+  useEffect(() => {
+    if (isMobileExtensive()) {
+      const hasSeenModal = localStorage.getItem('hasSeenMobileWarning')
+
+      if (hasSeenModal !== 'true') {
+        localStorage.setItem('hasSeenMobileWarning', 'true')
+        showModal()
+      } else if (accessToken !== localStorage.getItem('lastSeenToken')) {
+        accessToken && localStorage.setItem('lastSeenToken', accessToken)
+        showModal()
+      }
+    }
+  }, [accessToken])
+
+  return createPortal(
+    <Modal
+      footer={false}
+      className="p-0"
+      width={'398px'}
+      centered
+      styles={{
+        content: {
+          padding: 0,
+        },
+      }}
+      open={isModalOpen}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <div className="grid place-items-center px-6 py-20">
+        <SadIcon />
+        <p className="mt-10 text-center text-[32px] font-semibold leading-10">
+          Sorry, page is not mobile friendly
+        </p>
+        <p className="my-4 text-center text-gray-1">
+          LMS Pro can be difficult to use on a mobile device. We are going to
+          improve our website very soon.
+        </p>
+        <ButtonPrimary
+          title={'Continue'}
+          className="mt-10 h-10 w-[200px]"
+          onClick={handleCancel}
+        />
+      </div>
+    </Modal>,
+    document.body,
+  )
+}
+
+export default ModalMobile
