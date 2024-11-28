4d7bedb37435b0c7a6c4e843ffc3a817982a071f
andnh <25-sapp.fe.andnh@users.noreply.git.sapp.edu.vn>
2024-11-28 11:43:17 +0700

    fix: [SAPP-5840] help center not show up on mobile

--- src/components/mycourses/course-detail/CourseParts.tsx
+++ src/components/mycourses/course-detail/CourseParts.tsx
@@ -20,76 +20,78 @@ const CourseParts = ({
 }) => {
   const router = useRouter()
 
-  const cardRefs = useRef<any>([]); // Để lưu ref của các thẻ card
+  const cardRefs = useRef<any>([]) // Để lưu ref của các thẻ card
 
   // Scroll đến phần tử có id khớp với router.query.type
   useEffect(() => {
     const courseId = router.query.focus_id as string // Chuyển đổi giá trị query thành số (giả sử id là số)
-    const selectedCard = cardRefs.current[courseId];
+    const selectedCard = cardRefs.current[courseId]
     if (selectedCard) {
-      selectedCard.scrollIntoView({ behavior: "smooth", block: "center" });
+      selectedCard.scrollIntoView({ behavior: 'smooth', block: 'center' })
     }
-  }, [router?.query?.focus_id]);
+  }, [router?.query?.focus_id])
 
   return (
     <div
-      className={`${isEmpty(courses)
-        ? 'flex min-h-[calc(100vh-15rem)] items-center justify-center'
-        : 'mb-10 grid grid-cols-2 gap-6 2xl-min:grid-cols-3'
-        }`}
+      className={`${
+        isEmpty(courses)
+          ? 'flex min-h-[calc(100vh-15rem)] items-center justify-center'
+          : 'mb-10 grid gap-6 md:grid-cols-2 2xl-min:grid-cols-3'
+      }`}
     >
       {!isEmpty(courses) ? (
         courses?.map((coursePart, index: number) => {
           return (
-            <div key={coursePart?.id} ref={(el) => (cardRefs.current[coursePart.id] = el)}>
-              {
-                router?.query?.focus_id === coursePart.id ? (
-                  <div
-                    className={`item aspect-h-16 relative flex h-[412px] flex-col justify-between bg-white p-[30px] shadow-sidebar card active-section`}
-                    data-aos={ANIMATION.DATA_AOS}
-                    style={{ zIndex: courses?.length - index }}
-                    ref={lastElementRef}
-                  >
-                    {[
-                      TEST_TYPE.MID_TERM_TEST,
-                      TEST_TYPE.FINAL_TEST,
-                      TEST_TYPE.MOCK_TEST,
-                    ].includes(coursePart?.course_section_type as TEST_TYPE) ? (
-                      <PartMiddleTest
-                        key={index}
-                        coursePart={coursePart}
-                        is_passed_course={is_passed_course}
-                        class_user_id={class_user_id}
-                      />
-                    ) : (
-                      <Part key={index} course={coursePart} />
-                    )}
-                  </div>
-                ) : (
-                  <div
-                    key={coursePart?.id}
-                    className={`item aspect-h-16 relative flex h-[412px] flex-col justify-between bg-white p-[30px] shadow-sidebar`}
-                    ref={lastElementRef}
-                    data-aos={ANIMATION.DATA_AOS}
-                    style={{ zIndex: courses?.length - index }}
-                  >
-                    {[
-                      TEST_TYPE.MID_TERM_TEST,
-                      TEST_TYPE.FINAL_TEST,
-                      TEST_TYPE.MOCK_TEST,
-                    ].includes(coursePart?.course_section_type as TEST_TYPE) ? (
-                      <PartMiddleTest
-                        key={index}
-                        coursePart={coursePart}
-                        is_passed_course={is_passed_course}
-                        class_user_id={class_user_id}
-                      />
-                    ) : (
-                      <Part key={index} course={coursePart} />
-                    )}
-                  </div>
-                )
-              }
+            <div
+              key={coursePart?.id}
+              ref={(el) => (cardRefs.current[coursePart.id] = el)}
+            >
+              {router?.query?.focus_id === coursePart.id ? (
+                <div
+                  className={`item card active-section aspect-h-16 relative flex h-[412px] flex-col justify-between bg-white p-[30px] shadow-sidebar`}
+                  data-aos={ANIMATION.DATA_AOS}
+                  style={{ zIndex: courses?.length - index }}
+                  ref={lastElementRef}
+                >
+                  {[
+                    TEST_TYPE.MID_TERM_TEST,
+                    TEST_TYPE.FINAL_TEST,
+                    TEST_TYPE.MOCK_TEST,
+                  ].includes(coursePart?.course_section_type as TEST_TYPE) ? (
+                    <PartMiddleTest
+                      key={index}
+                      coursePart={coursePart}
+                      is_passed_course={is_passed_course}
+                      class_user_id={class_user_id}
+                    />
+                  ) : (
+                    <Part key={index} course={coursePart} />
+                  )}
+                </div>
+              ) : (
+                <div
+                  key={coursePart?.id}
+                  className={`item aspect-h-16 relative flex h-[412px] flex-col justify-between bg-white p-[30px] shadow-sidebar`}
+                  ref={lastElementRef}
+                  data-aos={ANIMATION.DATA_AOS}
+                  style={{ zIndex: courses?.length - index }}
+                >
+                  {[
+                    TEST_TYPE.MID_TERM_TEST,
+                    TEST_TYPE.FINAL_TEST,
+                    TEST_TYPE.MOCK_TEST,
+                  ].includes(coursePart?.course_section_type as TEST_TYPE) ? (
+                    <PartMiddleTest
+                      key={index}
+                      coursePart={coursePart}
+                      is_passed_course={is_passed_course}
+                      class_user_id={class_user_id}
+                    />
+                  ) : (
+                    <Part key={index} course={coursePart} />
+                  )}
+                </div>
+              )}
             </div>
           )
         })
