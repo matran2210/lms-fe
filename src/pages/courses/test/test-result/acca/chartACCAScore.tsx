235913d7cc63c63489a45d6523f62e9e19ce9cc4
longtq <longtq@aht-asia.com>
2024-12-16 17:07:27 +0700

    feat: sapp-6259 revise chart data

--- src/pages/courses/test/test-result/acca/chartACCAScore.tsx
+++ src/pages/courses/test/test-result/acca/chartACCAScore.tsx
@@ -1,7 +1,5 @@
 import Recommendation from '@components/test/Recommendation'
 import { calculatePercentage } from '@utils/helpers'
-import { formatNumber } from '@utils/formatNumber'
-import { isNull, isUndefined } from 'lodash'
 import { ChartDatum, IQuizAttempComment } from 'src/type'
 
 interface IProps {
@@ -22,33 +20,33 @@ const ChartACCAScore = ({ data, recommendation }: IProps) => {
         Multiple Choice Score by Part
       </div>
       <div className="flex-start dashboard-scroll-x flex w-full snap-x flex-row gap-14 scroll-smooth">
-        {data?.map((item: ChartDatum) => (
-          <div
-            key={item?.part_id}
-            className="flex w-11/12 max-w-78 shrink-0 snap-start flex-col items-start justify-end gap-2 md:w-1/2 xl:w-1/3"
-          >
-            <div className="line-clamp-2 font-normal text-bw-1">
-              {item?.title}
+        {data?.map((item: ChartDatum) => {
+          const percentage = calculatePercentage(
+            item?.section_score,
+            item?.max_section_score,
+          )
+          return (
+            <div
+              key={item?.part_id}
+              className="flex w-11/12 max-w-78 shrink-0 snap-start flex-col items-start justify-end gap-2 md:w-1/2 xl:w-1/3"
+            >
+              <div className="line-clamp-2 font-normal text-bw-1">
+                {item?.title}
+              </div>
+              <div className="relative h-2 w-full bg-gray-3">
+                <div
+                  className="absolute left-0 top-0 h-2 bg-primary"
+                  style={{
+                    width: `${percentage}%`,
+                  }}
+                ></div>
+              </div>
+              <div className="flex w-full items-center justify-end text-base font-normal text-bw-1">
+                {`${percentage}%`}
+              </div>
             </div>
-            <div className="relative h-2 w-full bg-gray-3">
-              <div
-                className="absolute left-0 top-0 h-2 bg-primary"
-                style={{
-                  width: `${calculatePercentage(
-                    item?.total_correct_answers,
-                    item?.total_questions,
-                  )}%`,
-                }}
-              ></div>
-            </div>
-            <div className="flex w-full items-center justify-end text-base font-normal text-bw-1">
-              {`${calculatePercentage(
-                item?.total_correct_answers,
-                item?.total_questions,
-              )}%`}
-            </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
       <div>
         {recommendation?.map((item, index) => (
--- src/pages/courses/test/test-result/cma/chartCMAScore.tsx
+++ src/pages/courses/test/test-result/cma/chartCMAScore.tsx
@@ -1,6 +1,6 @@
 import Recommendation from '@components/test/Recommendation'
 import { formatNumber } from '@utils/formatNumber'
-import { roundNumber } from '@utils/helpers'
+import { calculatePercentage, roundNumber } from '@utils/helpers'
 import { isNull, isUndefined } from 'lodash'
 import Image from 'next/image'
 import { ChartDatum, IQuizAttempComment } from 'src/type'
@@ -107,11 +107,10 @@ const ChartCMAScore = ({
                     <div
                       className="absolute bottom-0 w-14 border border-l-primary border-r-primary border-t-primary bg-secondary "
                       style={{
-                        height: `${
-                          (item?.total_correct_answers /
-                            item?.total_questions) *
-                          100
-                        }%`,
+                        height: `${calculatePercentage(
+                          item?.section_score,
+                          item?.max_section_score,
+                        )}%`,
                       }}
                     />
                     <div className="bg-black-1 absolute -bottom-2.5 left-1/2 h-2.5 w-[1px]" />
@@ -120,10 +119,7 @@ const ChartCMAScore = ({
                     {item?.short_name}
                   </div>
                   <div className="text-black-1 w-full text-center text-medium-sm font-normal">
-                    {`${roundNumber(
-                      (item?.total_questions / item?.total_quiz_questions) *
-                        100,
-                    )}%`}
+                    {`${roundNumber(item?.max_section_score)}%`}
                   </div>
                 </div>
               ))}
