0d1166893b5b65c6c669bc809f7f2120dc7f6303
huytu20 <tudh@sapp.edu.vn>
2025-02-04 16:59:59 +0700

    feat(SAPP-6913): ui new recommend

--- src/components/test/Recommendation.tsx
+++ src/components/test/Recommendation.tsx
@@ -1,6 +1,8 @@
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import React, { useState } from 'react'
 import { IQuizAttempComment } from 'src/type'
-import { ArrowUpIcon, IconDownArrow } from '@assets/icons'
+import EditorReader from '@components/base/editor/EditorReader'
+import ModalRecomment from './ModalRecomment'
+import SappIcon from 'src/common/SappIcon'
 
 interface IProps {
   data: IQuizAttempComment
@@ -8,58 +10,86 @@ interface IProps {
 }
 
 interface ICommentModal {
-  isOpen: boolean
-  setIsOpen: Dispatch<SetStateAction<boolean>>
   title: string
   content?: string
   className?: string
+  onSeeMore: () => void
 }
 
 const Recommendation = ({ data, key }: IProps) => {
-  const [isOpenComment, setIsComment] = useState<boolean>(true)
-  const [isOpenRecomment, setIsOpenRecomment] = useState<boolean>(true)
+  const [isOpenComment, setIsComment] = useState<boolean>(false)
+  const [isOpenRecomment, setIsOpenRecomment] = useState<boolean>(false)
+  const [currentContent, setCurrentContent] = useState<string | null>(null)
+
+  const handleOpenModal = (contentType: 'comment' | 'recommendation') => {
+    if (contentType === 'comment') {
+      setCurrentContent(data?.comment || '')
+      setIsComment(true)
+    } else {
+      setCurrentContent(data?.recommendation || '')
+      setIsOpenRecomment(true)
+    }
+  }
+
+  const handleCloseRecomment = () => {
+    setIsComment(false)
+    setIsOpenRecomment(false)
+  }
+
   return (
-    <div key={key} className=" text-[#050505]">
-      {data?.comment && (
-        <CommentModal
-          title="Examiner’s Comment:"
-          content={data?.comment}
-          isOpen={isOpenComment}
-          setIsOpen={setIsComment}
-          className="mt-5"
-        />
-      )}
-      {data?.recommendation && (
-        <CommentModal
-          title="Recommendation:"
-          content={data?.recommendation}
-          isOpen={isOpenRecomment}
-          setIsOpen={setIsOpenRecomment}
-        />
-      )}
-    </div>
+    <React.Fragment>
+      <div key={key} className="text-bw-1">
+        {data?.comment && (
+          <CommentModal
+            title="Examiner’s Comment:"
+            content={data?.comment}
+            className="mt-5"
+            onSeeMore={() => handleOpenModal('comment')}
+          />
+        )}
+        {data?.recommendation && (
+          <CommentModal
+            title="Recommendation:"
+            content={data?.recommendation}
+            onSeeMore={() => handleOpenModal('recommendation')}
+          />
+        )}
+      </div>
+      <ModalRecomment
+        isOpen={isOpenComment || isOpenRecomment}
+        handleCloseRecomment={handleCloseRecomment}
+        currentContent={currentContent}
+        isComment={isOpenComment}
+      />
+    </React.Fragment>
   )
 }
 
 const CommentModal = ({
-  isOpen,
-  setIsOpen,
   title,
   content,
   className,
+  onSeeMore,
 }: ICommentModal) => {
   return (
-    <div className={`mb-5 bg-gray-4 p-4 ${className ?? ''}`}>
+    <div className={`mb-6 bg-gray-4 p-4 ${className ?? ''}`}>
       <div className="flex items-center justify-between">
-        <h4 className="mb-1 font-medium">{title}</h4>
-        <button onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <IconDownArrow /> : <ArrowUpIcon />}
-        </button>
+        <div className="text-base font-medium">{title}</div>
+        <div
+          className="cursor-pointer text-sm text-state-info"
+          onClick={onSeeMore}
+        >
+          <div className="group flex items-center group-hover:cursor-pointer group-hover:fill-primary">
+            <div className="group-hover:text-primary">See more</div>
+            <SappIcon icon="seemore" />
+          </div>
+        </div>
       </div>
       <div
-        className={`${isOpen ? 'block' : 'hidden'} max-h-[300px]  overflow-y-auto break-words transition-all`}
-        dangerouslySetInnerHTML={{ __html: content ?? '' }}
-      />
+        className={`max-h-[210px] overflow-y-auto break-words transition-all`}
+      >
+        <EditorReader text_editor_content={content} className="mr-2" />
+      </div>
     </div>
   )
 }
