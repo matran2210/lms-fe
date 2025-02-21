bc1513437b5041d581fe7377a0eb8dc42654a36d
huytu20 <tudh@sapp.edu.vn>
2025-02-21 17:56:07 +0700

    feat(SAPP-11): hub form

--- src/components/base/modal/SappModalV3.tsx
+++ src/components/base/modal/SappModalV3.tsx
@@ -28,7 +28,7 @@ interface IProps {
   disabled?: boolean
   onOk: () => void
   classNameModal?: string | undefined
-  width?: number | undefined
+  width?: number | undefined | string
   handleClose?: () => void
   icon: ReactNode
   header: string
