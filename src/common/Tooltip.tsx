59aa66fc9cd4b23fb4c7e03021a217d7e1b3eb2c
andnh <25-sapp.fe.andnh@users.noreply.git.sapp.edu.vn>
2025-04-26 15:45:35 +0700

    fix: [SAPP-9355] tooltip color fix

--- src/common/Tooltip.tsx
+++ src/common/Tooltip.tsx
@@ -1,17 +1,29 @@
-import { Tooltip as AntdTooltip, TooltipProps } from 'antd'
+import { Tooltip as AntdTooltip, ConfigProvider, TooltipProps } from 'antd'
+
+export const DEFAULT_TOOLTIP_BG_COLOR = '#ffffff'
+export const DEFAULT_TOOLTIP_COLOR = '#000000'
 
 const Tooltip: React.FC<TooltipProps & { showTooltip?: boolean }> = ({
   showTooltip = true,
   children,
-  color = '#404041',
+  color = DEFAULT_TOOLTIP_BG_COLOR,
   ...props
 }) => {
-  return showTooltip ? (
-    <AntdTooltip color={color} {...props}>
-      {children}
-    </AntdTooltip>
-  ) : (
-    <>{children}</>
+  if (!showTooltip) return <div>{children}</div>
+
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorTextLightSolid: DEFAULT_TOOLTIP_COLOR,
+          colorText: DEFAULT_TOOLTIP_COLOR,
+        },
+      }}
+    >
+      <AntdTooltip color={color} {...props}>
+        {children}
+      </AntdTooltip>
+    </ConfigProvider>
   )
 }
 
