ac36698ad9319aba4ebe4fd5b572fc163e01ff8a
andnh <25-sapp.fe.andnh@users.noreply.git.sapp.edu.vn>
2025-04-29 15:57:43 +0700

    fix: [SAPP-9522] tooltip not working

--- src/components/mycourses/Heading.tsx
+++ src/components/mycourses/Heading.tsx
@@ -15,9 +15,7 @@ const Heading = ({ greeting, title, des }: IProps) => {
         <h1 className="line-clamp-1 text-2xl font-light text-bw-1">
           {greeting}
           <span className="ml-1.5 font-medium">
-            <Tooltip title={title} showTooltip={(title as string)?.length > 50}>
-              {truncateString(title, 80)}
-            </Tooltip>
+            <Tooltip title={title}>{truncateString(title, 80)}</Tooltip>
           </span>
         </h1>
       </div>
