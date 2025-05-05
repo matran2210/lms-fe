06427be8dc6fc1c50e30f97e6a3f33c8b5e25743
sonnh.sapp <sonnh@sapp.edu.vn>
2025-05-05 11:46:57 +0700

    fix: sapp-8843 hidden dashboard item in menu

--- src/components/mycourses/Course.tsx
+++ src/components/mycourses/Course.tsx
@@ -214,15 +214,19 @@ const Course = ({
       course?.course_type == 'NORMAL_COURSE' ||
       course?.course_type == 'PRACTICE_COURSE'
 
-    if (
-      isRedirectDashboard &&
-      (determineButtonToShow == BUTTON_STATUS.Review ||
-        determineButtonToShow == BUTTON_STATUS.Resume)
-    ) {
-      router.push(`/courses/my-course/${classInstance?.id}/dashboard`)
-    } else {
-      router.push(`/courses/my-course/${classInstance?.id}`)
-    }
+    // Tạm ẩn redirect dashboard begin
+    // if (
+    //   isRedirectDashboard &&
+    //   (determineButtonToShow == BUTTON_STATUS.Review ||
+    //     determineButtonToShow == BUTTON_STATUS.Resume)
+    // ) {
+    //   router.push(`/courses/my-course/${classInstance?.id}/dashboard`)
+    // } else {
+    //   router.push(`/courses/my-course/${classInstance?.id}`)
+    // }
+
+    router.push(`/courses/my-course/${classInstance?.id}`)
+    // Tạm ẩn redirect dashboard end
 
     if (isRedirectDashboard) {
       localStorage.setItem(
