1b0db95f35d0fa04a3744b2df63f9b67b1fefbe5
sonhero981 <sonhero981@gmail.com>
2025-05-11 14:36:36 +0700

    fix: config eslint

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,32 +1,61 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 
-/**
- * A shared ESLint configuration for the repository.
- *
- * @type {import("eslint").Linter.Config[]}
- * */
+const [tsParser, tsPlugin] = tseslint.configs.recommended;
+
 export const config = [
   js.configs.recommended,
   eslintConfigPrettier,
-  ...tseslint.configs.recommended,
+  tsParser,
+  tsPlugin,
+  pluginReact.configs.flat.recommended,
+
   {
-    plugins: {
-      turbo: turboPlugin,
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
-  },
-  {
     plugins: {
       onlyWarn,
+      "react-hooks": pluginReactHooks,
+      "@typescript-eslint": tseslint.plugin
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }],
+      "@typescript-eslint/no-unused-expressions": ["error", {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true
+      }]
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
+
   {
-    ignores: ["dist/**"],
+    ignores: ["dist/**", ".next/**", "node_modules/**"],
   },
 ];
