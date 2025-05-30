e60dbe6170a25fb2a35a8e3c8769e012bea2f425
sonnh.sapp <sonnh@sapp.edu.vn>
2025-05-30 11:35:32 +0700

    feat: update ui question answered

--- src/components/base/pagination/PageLink.tsx
+++ src/components/base/pagination/PageLink.tsx
@@ -74,7 +74,7 @@ const PageLink = ({
           ? 'min-h-8 min-w-8 rounded-md text-xsm font-semibold leading-4.8'
           : 'min-h-9.5 max-h-10 min-w-9.5 text-sm font-normal leading-5.5'
       } relative flex cursor-pointer items-center justify-center rounded p-2
-      ${isViewedProp && type !== 'row' ? '!border-default bg-gray-3' : ''} ${
+      ${isViewedProp && type !== 'row' ? 'bg-gray-12 text-white' : ''} ${
         active
           ? 'border-active bg-primary text-white'
           : !isViewedProp
