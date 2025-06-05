f7d2ef3fc3316b097ec2f86ec16a586e9072259a
andnh <25-sapp.fe.andnh@users.noreply.git.sapp.edu.vn>
2025-06-05 09:24:42 +0700

    fix: [SAPP-11127] update tailwind default color

--- src/common/TotalResullt.tsx
+++ src/common/TotalResullt.tsx
@@ -8,7 +8,7 @@ interface IProps {
 const TotalResullt = ({ total, className = '' }: IProps) => {
   return (
     <div
-      className={`border-r border-gray-1 pr-6 text-medium-sm font-normal text-gray-1 ${className}`}
+      className={`text-medium-sm border-r border-gray-1 pr-6 font-normal text-gray-1 ${className}`}
     >
       {`${total} ${total > 1 ? 'Results' : 'Result'}`}
     </div>
