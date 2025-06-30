0c003f8ba935c55598a891c36941b5d42350b36e
andnh <25-sapp.fe.andnh@users.noreply.git.sapp.edu.vn>
2025-06-30 17:01:38 +0700

    fix: [SAPP-11730] popup exam

--- src/pages/api/courses/index.ts
+++ src/pages/api/courses/index.ts
@@ -1,7 +1,8 @@
 import { fetcher } from '@services/requestV2'
 import url from 'src/redux/services/Course/MyCourse/Test/url'
 import { apiURL } from 'src/redux/services/httpService'
-import { IScoreDetails } from 'src/type'
+import { IResponse, IScoreDetails } from 'src/type'
+import { CourseDetail } from 'src/type/course'
 
 export class CoursesAPI {
   static getNoteDetail(
@@ -71,7 +72,7 @@ export class CoursesAPI {
     page_index: number,
     page_size: number,
     params: Object,
-  ): Promise<any> {
+  ): Promise<IResponse<CourseDetail>> {
     return fetcher(
       `courses/${id}?page_index=${page_index}&page_size=${page_size}`,
       {
