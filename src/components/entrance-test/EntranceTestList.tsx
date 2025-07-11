babb4764b0a49ba2b2d086ef2f33e0a89036867d
Lê Ngọc Hà <halh@sapp.edu.vn>
2025-07-11 18:12:11 +0700

    feat(sapp-12808): chỉnh lại logic hiển thị popup Take your test

--- src/components/entrance-test/EntranceTestList.tsx
+++ src/components/entrance-test/EntranceTestList.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import NoData from 'src/common/NoData'
 import { IEntranceTest } from 'src/type/entrance-test'
 import EntranceTest from './EntranceTest'
+import { EAttemptStatus } from 'src/constants/attempt'
 
 interface EntranceTestListProps {
   entranceTestLists: IEntranceTest[]
@@ -11,6 +12,13 @@ interface EntranceTestListProps {
 const EntranceTestList: React.FC<EntranceTestListProps> = ({
   entranceTestLists,
 }) => {
+  const firstIndexToShowEntrancePopup = entranceTestLists.findIndex(
+    (entrance) =>
+      entrance.is_attempt === false ||
+      (entrance.attempt_times < entrance.limit_count &&
+        entrance.attempt_status === EAttemptStatus.IN_PROGRESS),
+  )
+
   return (
     <div
       className={`${
@@ -21,19 +29,24 @@ const EntranceTestList: React.FC<EntranceTestListProps> = ({
       // data-aos={ANIMATION.DATA_AOS}
     >
       {!isEmpty(entranceTestLists) ? (
-        entranceTestLists?.map((e, index) => (
-          <div
-            key={index}
-            className={`item flex flex-col bg-white p-7.5 shadow-sidebar`}
-          >
-            <EntranceTest
-              data={e}
-              test_id_default={entranceTestLists.find(
-                (entrance) => entrance.is_attempt === false,
-              )}
-            />
-          </div>
-        ))
+        entranceTestLists?.map((e, index) => {
+          const isShowEntranceTestPopup =
+            firstIndexToShowEntrancePopup === index
+          return (
+            <div
+              key={index}
+              className={`item flex flex-col bg-white p-7.5 shadow-sidebar`}
+            >
+              <EntranceTest
+                data={e}
+                test_id_default={entranceTestLists.find(
+                  (entrance) => entrance.is_attempt === false,
+                )}
+                isShowEntranceTestPopup={isShowEntranceTestPopup}
+              />
+            </div>
+          )
+        })
       ) : (
         <NoData />
       )}
