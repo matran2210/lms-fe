adf8577ffda328bc5624b09ca659e1381f9de90f
thanhnd <thanhnd@sapp.edu.vn>
2025-07-21 14:36:23 +0700

    fix: remove popup block user click continue entrance test in second attempt

--- src/components/entrance-test/EntranceTest.tsx
+++ src/components/entrance-test/EntranceTest.tsx
@@ -9,7 +9,7 @@ import { trackGAEvent } from '@utils/google-analytics'
 import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { IEntranceTest } from 'src/type/entrance-test'
+import { Attempt, IEntranceTest } from 'src/type/entrance-test'
 import EntrancePopup from './EntrancePopup'
 import PopupExtend from './PopupExtend'
 
@@ -39,15 +39,24 @@ const EntranceTest = ({ data, test_id_default }: EntranceTestProps) => {
     useState<boolean>(false)
   const [remainingTimeLastAttempt, setRemainingTimeLastAttempt] =
     useState<number>(0)
+  const [currentAttempt, setCurrentAttempt] = useState<Attempt>(
+    data?.attempts?.[0] || ({} as Attempt),
+  )
+
+  useEffect(() => {
+    setCurrentAttempt(
+      data?.attempts?.[data?.attempts?.length - 1] || ({} as Attempt),
+    )
+  }, [data])
 
   useEffect(() => {
     if (data) {
       if (
         data?.quiz_timed &&
-        data?.attempt_status === EAttemptStatus['IN_PROGRESS']
+        currentAttempt?.status === EAttemptStatus['IN_PROGRESS']
       ) {
         const calcTime = dayjs(
-          dayjs(data?.created_at).add(data?.quiz_timed, 'minutes'),
+          dayjs(currentAttempt?.started_at).add(data?.quiz_timed, 'minutes'),
         ).diff(dayjs(), 'seconds')
 
         setRemainingTimeLastAttempt(calcTime >= 0 ? calcTime : 0)
@@ -66,7 +75,7 @@ const EntranceTest = ({ data, test_id_default }: EntranceTestProps) => {
         }
       }
     }
-  }, [data])
+  }, [data, currentAttempt])
 
   const timeTakenFormatted = data?.total_attempt_time
     ? formatTime(data?.total_attempt_time)
@@ -93,6 +102,17 @@ const EntranceTest = ({ data, test_id_default }: EntranceTestProps) => {
 
   const handleSubmit = async () => {
     //to do: start test
+    localStorage.removeItem('quizAttempt')
+    localStorage.setItem(
+      'quizAttempt',
+      JSON.stringify({
+        id: currentAttempt?.id,
+        number_of_attempts: data?.attempt_times,
+        is_limited: data?.is_limited,
+        quiz_timed: data?.quiz_timed,
+        created_at: data?.created_at,
+      }),
+    )
     try {
       router.push({
         pathname: `/test/${data?.id}`,
@@ -107,12 +127,11 @@ const EntranceTest = ({ data, test_id_default }: EntranceTestProps) => {
   const handleClickBegin = () => {
     //reset local storage
     localStorage.removeItem('quizAttempt')
-
-    if (data?.attempt_status === EAttemptStatus['IN_PROGRESS']) {
+    if (currentAttempt?.status === EAttemptStatus['IN_PROGRESS']) {
       localStorage.setItem(
         'quizAttempt',
         JSON.stringify({
-          id: data?.quiz_attempt_id,
+          id: currentAttempt?.id,
           number_of_attempts: data?.attempt_times,
           is_limited: data?.is_limited,
           quiz_timed: data?.quiz_timed,
@@ -169,9 +188,8 @@ const EntranceTest = ({ data, test_id_default }: EntranceTestProps) => {
         </div>
         <div className="action relative mt-10 flex items-center justify-between">
           {/* chưa làm bài hoặc đang làm bài thì button sẽ là begin */}
-          {!data?.attempt_status ||
-          data?.attempt_status === EAttemptStatus['IN_PROGRESS'] ||
-          data.attempts.length === 0 ? (
+          {!data?.attempts?.length ||
+          currentAttempt?.status === EAttemptStatus['IN_PROGRESS'] ? (
             <ButtonSecondary
               title="Begin"
               full={false}
@@ -241,7 +259,7 @@ const EntranceTest = ({ data, test_id_default }: EntranceTestProps) => {
         setOpenTest={setOpen}
       />
 
-      {data?.attempt_status === EAttemptStatus['IN_PROGRESS'] ? (
+      {currentAttempt?.status === EAttemptStatus['IN_PROGRESS'] ? (
         <SappModalV3
           title={
             <div className="flex items-center justify-between gap-2">
