7cc9e9ff94567e727bf410378adc44c491218b31
sonnh.sapp <sonnh@sapp.edu.vn>
2025-07-25 11:36:52 +0700

    fix: merged isses

--- src/components/base/pagination/PageLink.tsx
+++ src/components/base/pagination/PageLink.tsx
@@ -1,15 +1,14 @@
 import { FlagIcon } from '@assets/icons'
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import React from 'react'
 
-interface Props {
+interface PageLinkProps extends React.LiHTMLAttributes<HTMLLIElement> {
   active?: boolean
   disabled?: boolean
-  children?: any
-  onClick?: () => void
   arrow?: boolean
   type?: string
   isViewedProp?: boolean
   isFlagedProp?: boolean
+  children?: React.ReactNode
 }
 
 const PageLink = ({
@@ -21,13 +20,7 @@ const PageLink = ({
   isViewedProp,
   isFlagedProp,
   ...otherProps
-}: Props) => {
-  // const [isViewed, setIsViewed] = useState<boolean>(false)
-  // useEffect(() => {
-  //   if (isViewedProp !== undefined) {
-  //     setIsViewed(isViewedProp)
-  //   }
-  // }, [isViewedProp])
+}: PageLinkProps) => {
   if (arrow && disabled) {
     return (
       <li
@@ -40,20 +33,20 @@ const PageLink = ({
         {children}
       </li>
     )
-  } else if (arrow) {
+  }
+
+  if (arrow) {
     return (
-      <div className="cursor-pointer">
-        <li
-          className={`${
-            type === 'table'
-              ? 'min-h-8 min-w-8 text-[#7E8299]'
-              : 'min-h-10 cursor-pointer'
-          } flex cursor-pointer items-center justify-center`}
-          {...otherProps}
-        >
-          {children}
-        </li>
-      </div>
+      <li
+        className={`${
+          type === 'table'
+            ? 'min-h-8 min-w-8 text-[#7E8299]'
+            : 'min-h-10 cursor-pointer'
+        } flex cursor-pointer items-center justify-center`}
+        {...otherProps}
+      >
+        {children}
+      </li>
     )
   }
 
@@ -63,7 +56,7 @@ const PageLink = ({
         className={`flex items-center justify-center ${
           type === 'table'
             ? 'min-h-8 min-w-8 text-[#7E8299]'
-            : 'min-h-10 text-3xl font-thin leading-[33px] text-[#A1A1A1] '
+            : 'min-h-10 text-3xl font-thin leading-[33px] text-[#A1A1A1]'
         }`}
       >
         {children}
@@ -75,10 +68,11 @@ const PageLink = ({
     <li
       className={`${
         type === 'table'
-          ? 'text-xsm min-h-8 min-w-8 rounded-md font-semibold leading-[18px]'
+          ? 'min-h-8 min-w-8 rounded-md text-xsm font-semibold leading-[18px]'
           : 'min-h-9.5 max-h-10 min-w-[38px] text-sm font-normal leading-[22px]'
       } relative flex cursor-pointer items-center justify-center rounded p-2
-      ${isViewedProp && type !== 'row' ? 'bg-gray-400 text-white' : ''} ${
+      ${isViewedProp && type !== 'row' ? 'bg-gray-400 text-white' : ''} 
+      ${
         active
           ? 'border-[#FFB800] bg-primary text-white'
           : !isViewedProp
@@ -88,18 +82,10 @@ const PageLink = ({
       aria-current={active ? 'page' : undefined}
       {...otherProps}
     >
-      <span
-        className="h-[22px] w-4 text-center"
-        // onClick={() => {
-        //   setIsViewed(true)
-        //   // setIsFlagged(!isFlagged)
-        // }}
-      >
-        {children}
-      </span>
+      <span className="h-[22px] w-4 text-center">{children}</span>
       {isFlagedProp && (
         <div className="absolute -right-1 -top-[5px]">
-          <FlagIcon width={'16'} height={'16'} />
+          <FlagIcon width="16" height="16" />
         </div>
       )}
     </li>
