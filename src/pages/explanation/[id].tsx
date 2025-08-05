b4aee0c3c15c032f236329f59ab3edf223a084d4
Lê Ngọc Hà <halh@sapp.edu.vn>
2025-08-05 11:44:03 +0700

    feat(sapp-10306): update explanation-package-ha-test verson from 1.1.16-test into 1.1.17-test

--- src/pages/explanation/[id].tsx
+++ src/pages/explanation/[id].tsx
@@ -1,7 +1,7 @@
 import { AltArrowLeft, MenuDotsIcon } from '@assets/icons'
 import FullScreenLayout from '@components/layout/FullScreenLayout'
 import { LAYOUT } from '@utils/constants'
-import { ExplanationPackage } from 'explanation-package-ha-test'
+import { ExplanationPackageV2 } from 'explanation-package-ha-test'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import SappLoadingGlobal from 'src/common/SappLoadingGlobal'
@@ -179,7 +179,7 @@ const Explanation = () => {
             </button>
           </Tooltip>
         </div>
-        <ExplanationPackage
+        <ExplanationPackageV2
           getActiveQuestion={getActiveQuestion}
           activeQuestion={{
             ...activeQuestion,
