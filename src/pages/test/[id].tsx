039a2ffcc04fc6cdf141d055ac78867848158fbc
sonnh.sapp <sonnh@sapp.edu.vn>
2025-08-14 14:17:51 +0700

    fix: animation footer

--- src/pages/test/[id].tsx
+++ src/pages/test/[id].tsx
@@ -2593,9 +2593,8 @@ const TestDetail = () => {
               )}
               style={{
                 ['--footer-h' as any]: activeShowAll
-                  ? `${80 + Math.max(0, Math.ceil((filteredTabs?.length || 0) / 25) - 1) * 44}px`
+                  ? `${80 + Math.max(1, Math.ceil((filteredTabs?.length || 0) / 25) - 1) * 44}px`
                   : '80px',
-                minHeight: activeShowAll ? '124px' : '80px',
               }}
             >
               <div className="hidden h-full w-[150px] items-center gap-1 lg:flex">
