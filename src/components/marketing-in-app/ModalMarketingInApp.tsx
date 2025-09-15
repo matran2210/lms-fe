e25b048a4bf5aa62596fa737add78db66691eabf
Đỗ Trung Đạt <>
2025-09-15 10:10:14 +0700

    feat: sapp-15453 Responsive phần Modal Marketing in app

--- src/components/marketing-in-app/ModalMarketingInApp.tsx
+++ src/components/marketing-in-app/ModalMarketingInApp.tsx
@@ -4,6 +4,7 @@ import ButtonPrimary from '@components/base/button/ButtonPrimary'
 import ButtonText from '@components/base/button/ButtonText'
 import { Dispatch, SetStateAction } from 'react'
 import { useRouter } from 'next/router'
+import { useTailwindBreakpoint } from 'src/hooks/useTailwindBreakpoint'
 
 const ModalMarketingInApp = ({
   open,
@@ -13,9 +14,13 @@ const ModalMarketingInApp = ({
   setOpen: Dispatch<SetStateAction<boolean>>
 }) => {
   const router = useRouter()
+  const { isMobileView, isTabletView } = useTailwindBreakpoint()
+  const widthModal = isMobileView ? 343 : isTabletView ? 600 : 1000
+  const widthImg = isMobileView ? 343 : isTabletView ? 600 : 1000
+  const heightImg = isMobileView ? 170 : isTabletView ? 300 : 380
   return (
     <Modal
-      width={1000}
+      width={widthModal}
       footer={false}
       open={open}
       centered
@@ -26,34 +31,38 @@ const ModalMarketingInApp = ({
         <Image
           className="rounded-t-3xl"
           src="https://cdn.sapp.edu.vn/icons/img_header_modal_mkt_in_app.png"
-          width={1000}
-          height={380}
+          width={widthImg}
+          height={heightImg}
           alt="default_bg_mkt_in_app"
           priority
           placeholder="blur"
           blurDataURL="data:image/png;base64,..."
         />
       </div>
-      <div className="flex flex-col items-center justify-center px-[200px] py-[56px]">
-        <div className="justify-start self-stretch text-center text-[32px] font-bold leading-[46px] text-gray-800">
+      <div className="flex flex-col items-center justify-center p-6 md:p-8 lg:px-[200px] lg:py-[56px]">
+        <div className="self-stretch text-center text-base font-bold leading-7 text-gray-800 md:text-2xl md:leading-[34px] lg:text-[32px] lg:leading-[46px]">
           SAPP LMS has updated to a new version.
         </div>
-        <div className="mt-6 justify-center self-stretch text-center text-base font-normal leading-normal text-gray-800">
+
+        <div className="self-stretch text-center text-xs font-normal leading-normal text-gray-800 md:mt-4 md:text-base lg:mt-6">
           We’ve just upgraded to a brand-new version of SAPP LMS. This update
           brings a smoother interface, improved performance, and new features
           designed to make your learning journey easier and more engaging.
         </div>
-        <div className="mt-10 flex w-full flex-col items-center justify-center gap-3 px-[100px]">
+
+        <div className="flex w-full flex-col items-center justify-center gap-3 px-0 sm:px-6 md:mt-8 md:px-10 lg:mt-10 lg:px-[100px]">
           <ButtonPrimary
             title="Explore now"
-            className="w-full"
+            className="w-full lg:text-lg"
+            size={isMobileView || isTabletView ? 'small' : 'medium'}
             onClick={() => {
               router.push('/marketing-in-app')
             }}
           />
           <ButtonText
-            size="medium"
+            size={isMobileView || isTabletView ? 'small' : 'medium'}
             onClick={() => setOpen(false)}
+            className="lg:text-lg"
             title="Skip"
           />
         </div>
