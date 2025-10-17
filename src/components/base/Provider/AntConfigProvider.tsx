2cd3146d02a8f4180cd52236739696de8dbc3804
dungnq <dungnq1@sapp.edu.vn>
2025-10-17 17:53:25 +0700

    feat: change z index ant-modal

--- src/components/base/Provider/AntConfigProvider.tsx
+++ src/components/base/Provider/AntConfigProvider.tsx
@@ -42,6 +42,12 @@ const AntConfigProvider = ({ children }: React.PropsWithChildren<{}>) => {
         },
       }}
     >
+      <style>
+        {`
+          .ant-modal-root .ant-modal-mask { z-index: 99999 !important; }
+          .ant-modal-root .ant-modal { z-index: 99999 !important; }
+        `}
+      </style>
       {children}
     </ConfigProvider>
   )
