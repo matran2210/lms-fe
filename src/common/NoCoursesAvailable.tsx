392bb54c199871fd0d828995b8912f5f1e6ad875
Đỗ Trung Đạt <>
2025-10-28 09:27:11 +0700

    feat: sapp-16902 [Học thử] FE check lại logic popup bị lock bên v1

--- src/common/NoCoursesAvailable.tsx
+++ src/common/NoCoursesAvailable.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ANIMATION } from 'src/constants'
 import { NoCoursesAvailableIcon } from '@components/icons'
 
 const NoCoursesAvailable = () => {
+
+  useEffect(() => {
+    document.body.classList.add('overflow-y-hidden')
+
+    return () =>
+      document.body.classList.remove('overflow-y-hidden')
+  }, [])
+
   return (
     <div
       data-aos={ANIMATION.DATA_AOS}
-      className="short:mt-[86px] mt-[176px] flex flex-col items-center justify-center"
+      className="short:mt-[-100px] flex flex-col items-center justify-center"
     >
       <NoCoursesAvailableIcon />
       <div className="text-center text-base font-normal leading-6 text-gray-1">
