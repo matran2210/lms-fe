b186fd73b54f315526d228be861ac581de4389ae
dungnq <dungnq1@sapp.edu.vn>
2025-11-10 17:44:33 +0700

    feat: [SAPP-17402] change size dot history item

--- src/components/profile/LoginHistory/HistoryItem.tsx
+++ src/components/profile/LoginHistory/HistoryItem.tsx
@@ -22,7 +22,7 @@ const HistoryItem = ({ data }: any) => {
       <div className="flex items-center gap-2 text-xs text-secondary-100 md:text-sm">
         <div>{formattedTime}</div>
         {data.location && (
-          <div className="mx-1 h-[4px] w-[4px] rounded-full bg-secondary-100"></div>
+          <div className="mx-1 size-1 rounded-full bg-secondary-100"></div>
         )}
         <div>{data.location || ''}</div>
       </div>
