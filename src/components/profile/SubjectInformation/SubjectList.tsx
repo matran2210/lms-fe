aff8adec77c26b2f0bd7ef84d474ae762fea5513
dungnq <dungnq1@sapp.edu.vn>
2025-11-11 11:06:27 +0700

    feat: [SAPP-17451] fix entrance test UI, animation subject list

--- src/components/profile/SubjectInformation/SubjectList.tsx
+++ src/components/profile/SubjectInformation/SubjectList.tsx
@@ -8,6 +8,7 @@ import Icon from '@components/icons'
 import { CollapseArrowIcon } from '@assets/icons'
 import clsx from 'clsx'
 import { PROGRAM } from 'src/constants'
+import { useTailwindBreakpoint } from 'src/hooks/useTailwindBreakpoint'
 
 export interface SubjectOptionItem {
   course_category_name: 'CMA' | 'CFA' | 'ACCA'
@@ -20,6 +21,7 @@ interface IProps {
   isEdit: boolean
 }
 const SubjectList = ({ isEdit }: IProps) => {
+  const { isAlwaysShowSidebar } = useTailwindBreakpoint()
   const [makeDefaultDrawer, setMakeDefaultDrawer] = useState<{
     status: boolean
     course_category_name: 'CMA' | 'CFA' | 'ACCA'
@@ -90,6 +92,8 @@ const SubjectList = ({ isEdit }: IProps) => {
         classNameHeader={'bg-white !text-black md:!p-0 lg:!px-8 lg:!py-6'}
         classNameBody="pt-0 lg:pt-2 md:!px-0 lg:!px-8"
         rootClassName={'profile-subject-drawer'}
+        placement={!isAlwaysShowSidebar ? 'bottom' : 'right'}
+        height="auto"
         classNames={{
           content: 'rounded-2xl',
         }}
