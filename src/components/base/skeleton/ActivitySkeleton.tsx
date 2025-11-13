94590fdf6ff1e484a810b9bcdc46871556ef238c
huytu20 <tudh@sapp.edu.vn>
2025-11-13 10:21:48 +0700

    feat(sapp-17546): add loading activity for mobile

--- src/components/base/skeleton/ActivitySkeleton.tsx
+++ src/components/base/skeleton/ActivitySkeleton.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode } from 'react'
-import { SkeletonProps } from './type'
 import clsx from 'clsx'
+import React from 'react'
+import { SkeletonProps } from './type'
 
 const ActivitySkeleton: React.FunctionComponent<SkeletonProps> = ({
   children,
@@ -19,14 +19,14 @@ const ActivitySkeleton: React.FunctionComponent<SkeletonProps> = ({
             <div
               key={index}
               role="status"
-              className={clsx('animate-pulse p-6', classChild)}
+              className={clsx('animate-pulse p-3 xl:p-6', classChild)}
             >
-              <div className="mb-6 h-60 bg-[#F1F1F1]"></div>
-              <div className="mb-3 h-7 bg-[#F1F1F1]"></div>
-              <div className="mb-6 h-4 bg-[#F1F1F1]"></div>
-              <div className="mb-3 h-7 bg-[#F1F1F1]"></div>
-              <div className="mb-6 h-4 bg-[#F1F1F1]"></div>
-              <div className="mb-3 h-[7.5rem] bg-[#F1F1F1]"></div>
+              <div className="mb-6 h-60 rounded-lg bg-gray-3"></div>
+              <div className="mb-3 h-7 rounded-lg bg-gray-3"></div>
+              <div className="mb-6 h-4 rounded-lg bg-gray-3"></div>
+              <div className="mb-3 h-7 rounded-lg bg-gray-3"></div>
+              <div className="mb-6 h-4 rounded-lg bg-gray-3"></div>
+              <div className="mb-3 h-[7.5rem] rounded-lg bg-gray-3"></div>
             </div>
           ))}
         </div>
