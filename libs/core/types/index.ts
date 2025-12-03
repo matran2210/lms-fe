f3fbad87b6f3f56268841ea542247d875584ad19
sonnh.sapp <sonnh@sapp.edu.vn>
2025-12-03 09:33:36 +0700

    fix: menu item

--- libs/core/types/index.ts
+++ libs/core/types/index.ts
@@ -163,6 +163,24 @@ export interface IPopupFormState {
   thankYou: boolean;
   thankYouLater: boolean;
 }
+export type MenuItem = {
+  name: string
+  icon: string
+  url: string
+  type?: string
+  id: string
+  depth: number
+  subItems?: MenuItem[]
+}
+
+export type MenuOption = {
+  name: string
+  icon: string
+  url: string
+  type: string
+  subItems?: MenuOption[]
+}
+
 
 export * from "./common";
 export * from "./Icon";
