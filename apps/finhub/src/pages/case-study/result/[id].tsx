d023ae47f8609bcde27fb87dbb5e632835360b63
Lê Ngọc Hà <halh@sapp.edu.vn>
2025-12-03 10:22:31 +0700

    fix: simplify state update logic for highlight buttons

--- apps/finhub/src/pages/case-study/result/[id].tsx
+++ apps/finhub/src/pages/case-study/result/[id].tsx
@@ -1011,8 +1011,8 @@ const CaseStudyResult = () => {
                 <button
                   className={`h-full ${allowUnHighLight && 'bg-yellow-300'}`}
                   onClick={() => {
-                    (setAllowUnHighLight(!allowUnHighLight),
-                      setAllowHighLight(false))
+                    setAllowUnHighLight(!allowUnHighLight)
+                    setAllowHighLight(false)
                   }}
                 >
                   <div className="flex items-center gap-3 border-l px-4 3xl:pe-6 3xl:ps-6 ">
