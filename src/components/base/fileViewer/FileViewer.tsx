9d279f15c2f8722b0807ab8c1d871f5f9bb61bc4
sonnh.sapp <sonnh@sapp.edu.vn>
2025-12-11 16:25:01 +0700

    fix: sapp-18746 implement file size check for download prompt in FileViewer component

--- src/components/base/fileViewer/FileViewer.tsx
+++ src/components/base/fileViewer/FileViewer.tsx
@@ -1,6 +1,8 @@
+import React, { useEffect, useState } from 'react'
 import { isPdfFile } from '@utils/helpers'
-import React from 'react'
 import { OFFICE_VIEWER_URL } from 'src/constants'
+
+const MAX_SIZE = 20 * 1024 * 1024
 
 const FileViewer = ({
   fileName,
@@ -9,6 +11,31 @@ const FileViewer = ({
   fileName: string
   fileUrl: string
 }) => {
+  const [shouldDownload, setShouldDownload] = useState(false)
+
+  useEffect(() => {
+    const checkSize = async () => {
+      try {
+        const res = await fetch(fileUrl, { method: 'HEAD' })
+        const size = res.headers.get('content-length')
+
+        if (size && Number(size) > MAX_SIZE) {
+          setShouldDownload(true)
+        }
+      } catch (err) {}
+    }
+
+    checkSize()
+  }, [fileUrl])
+
+  if (shouldDownload) {
+    window.location.href = fileUrl
+
+    return (
+      <div className="p-4">File lớn hơn 20MB. Vui lòng tải xuống để xem.</div>
+    )
+  }
+
   return (
     <>
       {isPdfFile(fileName) ? (
