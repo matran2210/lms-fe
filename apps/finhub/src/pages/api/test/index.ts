6a0de766800b78e6dcc6f11a740e9964ba712cf4
Lê Ngọc Hà <halh@sapp.edu.vn>
2025-12-19 14:10:26 +0700

    fix(sapp-17999): update getQuestionAnswer from testApi to TestServiceApi

--- apps/finhub/src/pages/api/test/index.ts
+++ apps/finhub/src/pages/api/test/index.ts
@@ -1,14 +1,6 @@
 import { fetcher } from '@services/requestV2'
 
 export class TestAPI {
-  static getQuestionAnswer(id: string) {
-    return fetcher(`question/results`, {
-      params: {
-        question_ids: id,
-      },
-    })
-  }
-
   static getResource(id: string) {
     return fetcher(`resource/${id}`)
   }
