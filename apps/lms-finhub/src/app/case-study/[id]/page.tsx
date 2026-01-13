48ea5df7d93a880ca896140f52d4bac8dc32325c
huytu20 <tudh@sapp.edu.vn>
2026-01-13 12:14:27 +0700

    refactor: remove unsaved changes warning logic from CaseStudyDetail component

--- apps/lms-finhub/src/app/case-study/[id]/page.tsx
+++ apps/lms-finhub/src/app/case-study/[id]/page.tsx
@@ -890,25 +890,7 @@ const CaseStudyDetail = () => {
   // }, [openScratchPad])
   const warningText =
     'You have unsaved changes - are you sure you wish to leave this page?'
-  useEffect(() => {
-    const handleWindowClose = (e: any) => {
-      if (!unsavedChanges) return
-      e.preventDefault()
-      return (e.returnValue = warningText)
-    }
-    const handleBrowseAway = () => {
-      if (!unsavedChanges) return
-      if (window.confirm(warningText)) return
-      router.events.emit('routeChangeError')
-      throw 'routeChange aborted.'
-    }
-    window.addEventListener('beforeunload', handleWindowClose)
-    router.events.on('routeChangeStart', handleBrowseAway)
-    return () => {
-      window.removeEventListener('beforeunload', handleWindowClose)
-      router.events.off('routeChangeStart', handleBrowseAway)
-    }
-  }, [unsavedChanges])
+
   useEffect(() => {
     if (startResize) {
       document.body.style.webkitUserSelect = 'none'
