9b0781183cd4e2f7c7678b314faf1b9ced211d13
dungnq <dungnq1@sapp.edu.vn>
2026-01-15 14:43:49 +0700

    fix: allow preview file MP3 in class resource teacher

--- apps/lms-pro/src/components/teacher/myClass/class-detail/ClassResourceTeacher.tsx
+++ apps/lms-pro/src/components/teacher/myClass/class-detail/ClassResourceTeacher.tsx
@@ -1,6 +1,7 @@
 import ClassResourceTeacherFilter from '@components/teacher/components/ClassResourceTeacherFilter'
 import NameNoActionCell from '@components/teacher/components/NameNoActionCell'
 import { CloseIcon, DownloadIcon } from '@lms/assets'
+import { useFeature } from '@lms/contexts'
 import {
   CLASS_SUFFIX_TYPE,
   ClassKey,
@@ -30,6 +31,7 @@ import { useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 export default function ClassResourceTeacher() {
+  const { videoUrl } = useFeature()
   const router = useRouter()
   const internalRef = useRef<HTMLVideoElement>(null)
   const { control, reset, getValues } = useForm()
@@ -196,11 +198,18 @@ export default function ClassResourceTeacher() {
   const renderPreviewContent = (resource: IClassResource) => {
     switch (resource.suffix_type) {
       case 'VIDEO':
+      case 'AUDIO':
         return (
           <SAPPVideo
             isFetchCaptions={false}
             streamRef={internalRef}
-            options={{ src: resource.sub_url }}
+            options={{
+              src: resource.url
+                ? resource.url
+                    .replace(videoUrl || '', '')
+                    .replace('/manifest/video.m3u8', '')
+                : resource.sub_url,
+            }}
           ></SAPPVideo>
         )
       case 'SHEET':
