174d4602942b14041347d7aacc1c520cca44a2ac
Đỗ Trung Đạt <>
2026-02-05 15:50:04 +0700

    feat: sapp-20802 update điều kiện action view course detail

--- features/courses/src/components/mycourses/Course.tsx
+++ features/courses/src/components/mycourses/Course.tsx
@@ -381,7 +381,7 @@ const Course = ({
     }
 
     // Handle default case
-    if (!isCanceled) {
+    if (!isCanceled && determineButtonToShow !== "Disabled") {
       handleCourseDetail();
     }
   };
