6883db7c4cf1f7dead5ae0d49c4ce142106d79fe
Lê Ngọc Hà <halh@sapp.edu.vn>
2026-03-10 16:14:53 +0700

    fix: fix lỗi build & change icon in quiz

--- apps/lms-pro/src/components/storyline/blocks/QuizBlock.tsx
+++ apps/lms-pro/src/components/storyline/blocks/QuizBlock.tsx
@@ -1151,7 +1151,7 @@ const QuizBlock = ({
           <>
             <div className="flex flex-1 items-center gap-2">
               <Image
-                src="/assets/images/pointerIcon.png"
+                src="/assets/images/findAnswerIcon.png"
                 alt="pointer"
                 width={24}
                 height={24}
