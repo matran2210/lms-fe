d8522352e507cf1c56c25a1ed60901100b352400
thanhnd <thanhnd@sapp.edu.vn>
2026-03-26 08:49:08 +0700

    feat: update security header in next config file

--- apps/lms-zoom/next.config.js
+++ apps/lms-zoom/next.config.js
@@ -16,7 +16,7 @@ const securityHeaders = [
   { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
   { key: 'X-Content-Type-Options', value: 'nosniff' },
   { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-  { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+  { key: 'Cross-Origin-Embedder-Policy', value: 'credentialless' },
 ]
 
 let nextConfig = {
