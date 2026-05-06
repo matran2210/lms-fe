4c851582b8935edc727c49aead9c88ac1433fff0
huytu20 <tudh@sapp.edu.vn>
2026-05-06 09:38:46 +0700

    refactor(sapp-23548): consolidate sent and updated dates

--- features/schedule/src/components/teacher/my-request/schedule-request/TableContainer.tsx
+++ features/schedule/src/components/teacher/my-request/schedule-request/TableContainer.tsx
@@ -177,7 +177,7 @@ export default function TableContainer({
             index +
             1 +
             ((pagination?.current || 1) - DEFAULT_PAGE_NUMBER) *
-              (pagination?.pageSize || DEFAULT_PAGE_SIZE)
+            (pagination?.pageSize || DEFAULT_PAGE_SIZE)
           }
           className="!text-zinc-400"
         />
@@ -226,21 +226,28 @@ export default function TableContainer({
       title: "Start Date - End Date",
       render: (_, record: IScheduleRequestItem) => (
         <TableCell
-          data={`${record?.schedule_time.start_date ? formatDateFromUTC(record?.schedule_time.start_date) : "-"} - ${
-            record?.schedule_time.end_date
-              ? formatDateFromUTC(record?.schedule_time.end_date)
-              : "-"
-          }`}
+          data={`${record?.schedule_time.start_date ? formatDateFromUTC(record?.schedule_time.start_date) : "-"} - ${record?.schedule_time.end_date
+            ? formatDateFromUTC(record?.schedule_time.end_date)
+            : "-"
+            }`}
         />
       ),
     },
     {
-      title: "Sent Date",
+      title: "Date",
       render: (_, record: IScheduleRequestItem) => (
-        <TableCell
-          data={formatDateFromUTC(record?.created_at)}
-          className="!text-zinc-400"
-        />
+        <>
+          {record?.created_at && (
+            <div>
+              Send Date: {formatDateFromUTC(record?.created_at)}
+            </div>
+          )}
+          {record?.updated_at && (
+            <div>
+              Updated Date: {formatDateFromUTC(record?.updated_at)}
+            </div>
+          )}
+        </>
       ),
     },
     {
@@ -248,15 +255,6 @@ export default function TableContainer({
       render: (_, record: IScheduleRequestItem) => (
         <TableCell
           data={record?.staff_detail?.full_name}
-          className="!text-zinc-400"
-        />
-      ),
-    },
-    {
-      title: "Update Date",
-      render: (_, record: IScheduleRequestItem) => (
-        <TableCell
-          data={formatDateFromUTC(record?.updated_at)}
           className="!text-zinc-400"
         />
       ),
@@ -353,7 +351,7 @@ export default function TableContainer({
     requestId,
     type,
     reason = "",
-    callback = () => {},
+    callback = () => { },
   }: UpdateStatusParams) => {
     try {
       /**
